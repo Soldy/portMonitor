16852e608b88842f197d589eacb9e8ad319e9bc6
Soldy <4786022+Soldy@users.noreply.github.com>
2019-09-22 10:45:49 +0100

    async ping
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
-const socket = new (require('net')).Socket();
+const net = require('net');
 
 exports.portTest = function (host, port, func){
     return new Promise((resolve => {
-        const end = (result,func) => {
+        let socket = new net.Socket();
+        let end = (result,func) => {
              socket.destroy();
              if(typeof socket !== "undefined")
                  socket.end();
@@ -14,6 +15,28 @@ exports.portTest = function (host, port, func){
         socket.setTimeout(4000);
         socket.once('error', ()=>end(false));
         socket.once('timeout', ()=>end(false));
+        socket.connect(port, host, ()=>end(true));
+    }));
+};
+
+exports.portTestPing = function (host, port){
+    return new Promise((resolve => {
+        let socket = new net.Socket();
+        let timeStart =0;
+        let timeEnd = 0
+        let end = (result) => {
+             timeEnd = Date.now();
+             socket.destroy();
+             if(typeof socket !== "undefined")
+                 socket.end();
+             if(result)
+                 resolve(timeEnd-timeStart);
+             resolve(-1);
+        };
+        socket.setTimeout(4000);
+        socket.once('error', ()=>end(false));
+        socket.once('timeout', ()=>end(false));
+        timeStart=Date.now();
         socket.connect(port, host, ()=>end(true));
     }));
 };
