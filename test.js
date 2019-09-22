38c8f8816367cb2eb06a40dc84fb485bb352dc24
Soldy <4786022+Soldy@users.noreply.github.com>
2019-09-22 01:07:32 +0100

    simple test
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test.js
+++ test.js
@@ -4,10 +4,55 @@ const nanoTest   = new (require("nanoTest")).test();
 const portTest   = require("./index.js").portTest;
 var ping =function(){
     nanoTest.add(
-       "check google",
+       "check nasa http",
+       {
+           "function":portTest,
+           "options":['nasa.com','80']
+       },
+       "==",
+       true 
+    );
+    nanoTest.add(
+       "check wikipedia https",
+       {
+           "function":portTest,
+           "options":['wikipedia.com','443']
+       },
+       "==",
+       true 
+    );
+    nanoTest.add(
+       "check facebook http",
+       {
+           "function":portTest,
+           "options":['facebook.com','80']
+       },
+       "==",
+       true 
+    );
+    nanoTest.add(
+       "check facebook https",
+       {
+           "function":portTest,
+           "options":['facebook.com','443']
+       },
+       "==",
+       true 
+    );
+    nanoTest.add(
+       "check google http",
        {
            "function":portTest,
            "options":['google.com','80']
+       },
+       "==",
+       true 
+    );
+    nanoTest.add(
+       "check google https",
+       {
+           "function":portTest,
+           "options":['google.com','443']
        },
        "==",
        true 
